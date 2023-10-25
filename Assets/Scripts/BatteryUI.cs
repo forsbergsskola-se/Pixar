c7f1493d4e288075b6c0591631b5b73ca7d65042
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-25 15:07:36 +0200

    Battery UI update

--- Assets/Scripts/BatteryUI.cs
+++ Assets/Scripts/BatteryUI.cs
@@ -3,16 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class BatteryUI : MonoBehaviour
 {
-    public UnityEvent light;
     public float batteryLevel;
     public Battery charge;
+
+    public Image BatteryColor;
+    void Start()
+    {
+        
+    }
 
     public void Update()
     {
         batteryLevel = charge.GetCharge();
-        
+
+        BatteryColor.fillAmount = batteryLevel / 100f;
+
+        if (batteryLevel < 20f)
+            BatteryColor.color = Color.red;
+        else if (batteryLevel < 50f)
+            BatteryColor.color = Color.yellow;
+        else
+            BatteryColor.color = Color.green;
     }
 }
