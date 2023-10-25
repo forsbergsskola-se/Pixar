22638e373e8295b18a1f5cadd201bc369c169cb3
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-25 13:21:52 +0200

    light on

---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    public SpriteRenderer lightBeam;
+    public void OnLampChange(Lamp lamp)
+    {
+        bool isTurnedOn = lamp.GetIsTurnedOn();
+        this.lightBeam.enabled = isTurnedOn;
+
+    }
+}
