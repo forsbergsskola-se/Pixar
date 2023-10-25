e6945ebd3c77ce9b64fb7eadb69f7e1dd2daba72
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-25 15:54:32 +0200

    Added Percentage UI

--- Assets/Scripts/BatteryUI.cs
+++ Assets/Scripts/BatteryUI.cs
@@ -1,33 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
 
 public class BatteryUI : MonoBehaviour
 {
     public float batteryLevel;
-    public Battery charge;
 
+    public Battery charge;
     public Image BatteryColor;
-    void Start()
-    {
-        
-    }
+
+    public Text batteryPercentage;
 
     public void Update()
     {
         batteryLevel = charge.GetCharge();
 
-        BatteryColor.fillAmount = batteryLevel / 100f;
-
-        if (batteryLevel < 20f)
+        if (batteryLevel < 20f) //Deciding color depending on battery level
             BatteryColor.color = Color.red;
         else if (batteryLevel < 50f)
             BatteryColor.color = Color.yellow;
         else
             BatteryColor.color = Color.green;
+
+        batteryPercentage.text = $"{batteryLevel:P}";
     }
 }
